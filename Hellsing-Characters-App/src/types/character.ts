727d2b395f8b1e4ce26be6d9f54eb85de0008758
Adria <adria_borrascarrillo@iescarlesvallbona.cat>
2026-03-14 14:04:57 +0100

    afegit fetch characters al app.tsx

---
+++ Hellsing-Characters-App/src/types/character.ts
@@ -0,0 +1,10 @@
+// types, descriu les propietats/estructura dels objectes
+
+export type character = {
+  _id: string
+  name: string
+  number: string
+  date: string
+  dies: boolean
+  aliases: []
+}
